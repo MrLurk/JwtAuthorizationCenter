4aabbb6a17b2fc6313bf2e55c350f5468792db80
JiangJin <m15675306819@163.com>
2020-10-17 21:07:46 +0800

    初始化授权中心

---
+++ JwtAuthorizationCenter/AuthorizationCenter/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AuthorizationCenter.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase {
+
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get() {
+            return Ok(1);
+        }
+    }
+}
